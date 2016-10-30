ca4bed19f9cf8b1486015028c097c6cbdc07d27d
Andrey <akella449@gmail.com>
2016-10-30 20:05:52 +0500

    Create Lab4(react).cs

---
+++ LP/Lab4(react).cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication4
+{
+    class React
+    {
+        public void Message1()
+        {
+            Console.WriteLine("Площадь стала равна единице");
+        }
+        public void Message2(int num)
+        {
+            Console.WriteLine("Вершина "+num+" имеет нулевую координату");
+        }
+    }
+}
